ca54c8be2c07392cb2b1647fbcd6caae5a71b53d
Jaap Koetsier <jaap@chariot-it.nl>
2020-09-09 11:26:27 +0200

    PGB-1370/PGB-1371 Add SOAP request functionality for KvK DataService

--- src/gobmessage/config.py
+++ src/gobmessage/config.py
@@ -1,8 +1,29 @@
 import os
 
+
+def _getenv(varname, default_value=None, is_optional=False):
+    """
+    Returns the value of the environment variable "varname"
+    or the default value if the environment variable is not set
+
+    :param varname: name of the environment variable
+    :param default_value: value to return if variable is not set
+    :raises AssertionError: if variable not set or value is empty
+    :return: the value of the given variable
+    """
+    value = os.getenv(varname, default_value)
+    assert is_optional or value, f"Environment variable '{varname}' not set or empty"
+    return value
+
+
 GOB_MESSAGE_PORT = os.getenv('GOB_MESSAGE_PORT', default=8167)
 API_BASE_PATH = os.getenv("BASE_PATH", default="")
 
 MESSAGE_EXCHANGE = "gob.message"
 HR_MESSAGE_QUEUE = f"{MESSAGE_EXCHANGE}.hr"
 HR_MESSAGE_KEY = f"message.mutation.hr"
+
+# Optional parameters
+HR_KEYFILE = _getenv("HR_KEYFILE", is_optional=True)
+HR_CERTFILE = _getenv("HR_CERTFILE", is_optional=True)
+KVK_DATASERVICE_ADDRESS = _getenv("KVK_DATASERVICE_ADDRESS", is_optional=True)
---
+++ src/gobmessage/hr/kvk_dataservice/service.py
@@ -0,0 +1,74 @@
+import requests
+from datetime import datetime
+from zeep import Client
+from zeep.transports import Transport
+
+from gobmessage.config import HR_KEYFILE, HR_CERTFILE, KVK_DATASERVICE_ADDRESS
+from gobmessage.hr.kvk_dataservice.binary_signature import KvkDataServiceBinarySignature
+
+
+class KvkDataService:
+    """Encapsulates SOAP requests with the KvK DataService
+
+    Documentation: https://www.kvk.nl/sites/aansluitendataservice/index.html#/
+    """
+
+    wsdl = 'http://schemas.kvk.nl/contracts/kvk/dataservice/catalogus/2015/02/KVK-KvKDataservice.wsdl'
+
+    def _get_client(self):
+        """Returns the Zeep client, configured for use with the KvK DataService
+
+        :return:
+        """
+        session = requests.Session()
+        session.cert = (HR_CERTFILE, HR_KEYFILE)
+
+        transport = Transport(session=session)
+
+        client = Client(self.wsdl, wsse=KvkDataServiceBinarySignature(HR_KEYFILE, HR_CERTFILE), transport=transport)
+
+        # Replace the address, as the WSDL contains example.com as address.
+        client.service._binding_options['address'] = KVK_DATASERVICE_ADDRESS
+
+        return client
+
+    def _generate_reference(self):
+        """Generates a references to be used in the request.
+
+        :return:
+        """
+        dt = datetime.utcnow()
+        return f"GOB-{dt.strftime('%Y%m%d%H%M%S%f')}"
+
+    def _make_request(self, action: str, **kwargs):
+        """Makes a request to :action: with provided kwargs added to the request data
+
+        :param action:
+        :param kwargs:
+        :return:
+        """
+        client = self._get_client()
+
+        request_data = {
+            'klantreferentie': self._generate_reference(),
+            **kwargs
+        }
+
+        # Strict mode is not supported by KvK DataService
+        with client.settings(strict=False):
+            return getattr(client.service, action)(**request_data)
+
+    def ophalen_inschrijving_by_kvk_nummer(self, kvk_nummer):
+        return self._make_request('ophalenInschrijving', kvkNummer=kvk_nummer)
+
+    def ophalen_inschrijving_by_rsin(self, rsin):
+        return self._make_request('ophalenInschrijving', rsin=rsin)
+
+    def ophalen_vestiging_by_vestigingsnummer(self, vestigingsnummer):
+        return self._make_request('ophalenVestiging', vestigingsnummer=vestigingsnummer)
+
+    def ophalen_vestiging_by_kvk_nummer(self, kvk_nummer):
+        return self._make_request('ophalenVestiging', kvkNummer=kvk_nummer)
+
+    def ophalen_vestiging_by_rsin(self, rsin):
+        return self._make_request('ophalenVestiging', rsin=rsin)
