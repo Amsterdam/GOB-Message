ddeb14afbfaa34857267c3f85503089232b6db7f
Jaap Koetsier <jaap@chariot-it.nl>
2020-12-03 14:16:48 +0100

    PGB-2677 Fix Fetch HR Data on new update message. Upgrade Python 3.9

--- src/tests/hr/test_endpoint.py
+++ src/tests/hr/test_endpoint.py
@@ -1,16 +1,19 @@
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 from gobmessage.hr.endpoint import hr_endpoint, MESSAGE_EXCHANGE, HR_MESSAGE_KEY
 
 
 class TestEndpoint(TestCase):
 
-    @patch("gobmessage.hr.endpoint.publish")
-    @patch("gobmessage.hr.endpoint.request")
     @patch("gobmessage.hr.endpoint.Response", lambda x: x)
-    def test_hr_endpoint(self, mock_request, mock_publish):
+    def test_hr_endpoint(self):
+        mock_request = MagicMock()
+        mock_publish = MagicMock()
 
-        mock_request.data = b'Some data'
-        self.assertEqual('OK. Message received. Thank you, good bye.', hr_endpoint())
-        mock_publish.assert_called_with(MESSAGE_EXCHANGE, HR_MESSAGE_KEY, {'contents': 'Some data'})
+        with patch("gobmessage.hr.endpoint.publish", mock_publish), \
+            patch("gobmessage.hr.endpoint.request", mock_request):
+
+            mock_request.data = b'Some data'
+            self.assertEqual('OK. Message received. Thank you, good bye.', hr_endpoint())
+            mock_publish.assert_called_with(MESSAGE_EXCHANGE, HR_MESSAGE_KEY, {'contents': 'Some data'})
