34be9edf12f27288772d6fe51e3b6655b7027768
Jaap Koetsier <jaap@chariot-it.nl>
2021-01-19 11:49:21 +0100

    Move KvkUpdateBericht to separate file

--- src/gobmessage/hr/message.py
+++ src/gobmessage/hr/message.py
@@ -1,54 +1,6 @@
-from xml.etree import ElementTree
-
-from zeep import ns
-
 from gobmessage.database.repository import KvkUpdateMessages
 from gobmessage.database.session import DatabaseSession
 from gobmessage.hr.kvk_dataservice.service import KvkDataService
-
-
-class KvkUpdateBericht:
-    """Very basic parser of a KvK UpdateBericht
-
-    Input XML is a SOAP message with WSA and WSSE headers, but these are ignored for now. We simply get the information
-    we need from the Body.
-    """
-    namespaces = {
-        'soapenv': ns.SOAP_ENV_11,
-        'wsa': ns.WSA,
-        'wsse': ns.WSSE,
-        'dgl': 'http://www.digilevering.nl/digilevering.xsd',
-        'kvkupdate': 'http://schemas.kvk.nl/schemas/hrip/update/2018/01',
-        'kvkbericht': 'http://schemas.kvk.nl/schemas/hrip/bericht/2018/01',
-    }
-
-    def __init__(self, msg: str):
-        self.xmltree = ElementTree.fromstring(msg)
-
-    def get_kvk_nummer(self):
-        elm = self.xmltree.find(
-            "."
-            "/gebeurtenisinhoud"
-            "/{%s}UpdateBericht" % self.namespaces['kvkupdate'] + ""
-            "/{%s}heeftBetrekkingOp" % self.namespaces['kvkbericht'] + ""
-            "/{%s}kvkNummer" % self.namespaces['kvkbericht']
-        )
-
-        if elm is not None:
-            return elm.text
-
-    def get_vestigingsnummer(self):
-        elm = self.xmltree.find(
-            "."
-            "/gebeurtenisinhoud"
-            "/{%s}UpdateBericht" % self.namespaces['kvkupdate'] + ""
-            "/{%s}heeftBetrekkingOp" % self.namespaces['kvkbericht'] + ""
-            "/{%s}wordtUitgeoefendIn" % self.namespaces['kvkbericht'] + ""
-            "/{%s}vestigingsnummer" % self.namespaces['kvkbericht'] + ""
-        )
-
-        if elm is not None:
-            return elm.text
 
 
 def hr_message_handler(msg: dict):
--- src/tests/hr/test_message.py
+++ src/tests/hr/test_message.py
@@ -1,46 +1,7 @@
 from unittest import TestCase
-from unittest.mock import patch, call, MagicMock
+from unittest.mock import MagicMock, call, patch
 
-from gobmessage.hr.message import hr_message_handler, KvkUpdateBericht
-
-
-@patch("gobmessage.hr.message.ElementTree", MagicMock())
-class TestKvkUpdateBericht(TestCase):
-
-    def test_get_methods(self):
-        message = KvkUpdateBericht('some xml string')
-
-        testcases = [
-            (
-                "get_kvk_nummer",
-                "."
-                "/gebeurtenisinhoud"
-                "/{http://schemas.kvk.nl/schemas/hrip/update/2018/01}UpdateBericht"
-                "/{http://schemas.kvk.nl/schemas/hrip/bericht/2018/01}heeftBetrekkingOp"
-                "/{http://schemas.kvk.nl/schemas/hrip/bericht/2018/01}kvkNummer"),
-            (
-                "get_vestigingsnummer",
-                "."
-                "/gebeurtenisinhoud"
-                "/{http://schemas.kvk.nl/schemas/hrip/update/2018/01}UpdateBericht"
-                "/{http://schemas.kvk.nl/schemas/hrip/bericht/2018/01}heeftBetrekkingOp"
-                "/{http://schemas.kvk.nl/schemas/hrip/bericht/2018/01}wordtUitgeoefendIn"
-                "/{http://schemas.kvk.nl/schemas/hrip/bericht/2018/01}vestigingsnummer"
-            ),
-        ]
-
-        for method_name, xpath in testcases:
-            message.xmltree.find.return_value = MagicMock()
-
-            # Should return result
-            res = getattr(message, method_name)()
-            message.xmltree.find.assert_called_with(xpath)
-            self.assertEqual(res, message.xmltree.find().text)
-
-            # No result
-            message.xmltree.find.return_value = None
-            res = getattr(message, method_name)()
-            self.assertIsNone(res)
+from gobmessage.hr.message import hr_message_handler
 
 
 class TestMessage(TestCase):
