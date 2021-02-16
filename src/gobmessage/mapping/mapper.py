465f3be8fd2e49089659ff4bb24b3480c61f66e6
Jaap Koetsier <jaap@chariot-it.nl>
2021-02-16 11:21:16 +0100

    PGB-2944 Add 'communicatiegegevens' to hr mac

--- src/gobmessage/mapping/mapper.py
+++ src/gobmessage/mapping/mapper.py
@@ -1,6 +1,15 @@
 from abc import ABC, abstractmethod
 
 
+def get_value(d: dict, key: str):
+    if not isinstance(d, dict):
+        return None
+    if "." in key:
+        head, *tail = key.split('.')
+        return get_value(d.get(head, {}), '.'.join(tail))
+    return d.get(key)
+
+
 class Mapper(ABC):
 
     @property
@@ -29,21 +38,36 @@ class Mapper(ABC):
         pass
 
     def map(self, source: dict) -> dict:
-        def get_value(d: dict, key: str):
-            if not isinstance(d, dict):
-                return None
-            if "." in key:
-                head, *tail = key.split('.')
-                return get_value(d.get(head, {}), '.'.join(tail))
-            return d.get(key)
+        def map(src, fields: dict):
+            """
 
-        result = {}
-        for field_name, source_field in self.fields.items():
-            if isinstance(source_field, tuple):
-                result[field_name] = source_field[0](get_value(source, source_field[1]))
-            else:
-                result[field_name] = get_value(source, source_field)
-        return result
+            :param src: dict or str
+            :param fields:
+            :return:
+            """
+            result = {}
+            for field_name, source_field in fields.items():
+                if source_field == ".":
+                    result[field_name] = src
+                elif isinstance(source_field, tuple):
+                    result[field_name] = source_field[0](get_value(src, source_field[1]))
+                elif isinstance(source_field, dict):
+                    submapping = {k: v for k, v in source_field.items() if not k.startswith('_')}
+
+                    if source_field.get('_list', False):
+                        assert '_base' in source_field, "Should have _base in combination with _list"
+
+                        result[field_name] = [
+                            map(item, submapping)
+                            for item in get_value(src, source_field['_base'])
+                        ]
+                    else:
+                        raise NotImplementedError("Not sure what you try to accomplish here.")
+
+                else:
+                    result[field_name] = get_value(src, source_field)
+            return result
+        return map(source, self.fields)
 
     def get_id(self, mapped_entity: dict) -> str:
         return mapped_entity.get(self.entity_id)
--- src/tests/mapping/test_mapper.py
+++ src/tests/mapping/test_mapper.py
@@ -13,6 +13,12 @@ class MapperTestImpl(Mapper):
         'field 2': 'a.b',
         'field 3': 'e',
         'field 4': 'e.f',
+        'field 5': {
+            '_base': 'g.h',
+            '_list': True,
+            'field 6': 'i',
+            'field 7': 'j',
+        },
     }
 
 
@@ -31,6 +37,18 @@ class TestMapper(TestCase):
                 },
             },
             'e': 'E',
+            'g': {
+                'h': [
+                    {
+                        'i': 'I1',
+                        'j': 'J1',
+                    },
+                    {
+                        'i': 'I2',
+                        'j': 'J2',
+                    },
+                ]
+            },
         }
 
         m = MapperTestImpl()
@@ -39,9 +57,21 @@ class TestMapper(TestCase):
             'field 2': 'AB',
             'field 3': 'E',
             'field 4': None,
+            'field 5': [
+                {'field 6': 'I1', 'field 7': 'J1'},
+                {'field 6': 'I2', 'field 7': 'J2'},
+            ],
         }
 
         self.assertEqual(expected, m.map(source))
+
+        del MapperTestImpl.fields['field 5']['_list']
+
+        with self.assertRaises(NotImplementedError):
+            m.map(source)
+
+        # Put back
+        MapperTestImpl.fields['field 5']['_list'] = True
 
     def test_get_id(self):
         """Tests parent method from Mapper
