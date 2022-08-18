eb7cd67cde1224d1a681bbf282d9056cdbcc24e6
Peter Adrichem <Peter.Adrichem@gmail.com>
2022-08-18 16:28:59 +0200

    Test run cleanup.

--- src/test.sh
+++ src/test.sh
@@ -6,14 +6,11 @@ set -e # stop on any error
 # Coverage 6: coverage run --data-file=/tmp/.coveragerc …
 export COVERAGE_FILE=/tmp/.coverage
 
-# Clear any cached results.
-#find . -name "*.pyc" -exec rm -f {} \;
-
 echo "Running style checks"
 flake8
 
 echo "Running unit tests"
-coverage run --source=gobmessage --module pytest tests/
+coverage run --source=./gobmessage --module pytest tests/
 
 echo "Coverage report"
 coverage report --show-missing --fail-under=100
